6e75dfe5f6493af5b7b3830d25a4cf7c932d0f54
d460 <dagovf@comunidad.unam.mx>
2016-02-06 15:54:23 -0600

    Toolbar propio commit Android Studio

--- AttendanceApp/app/src/main/java/com/d460/attendanceapp/activities/PreferenceActivity.java
+++ AttendanceApp/app/src/main/java/com/d460/attendanceapp/activities/PreferenceActivity.java
@@ -1,32 +1,22 @@
 package com.d460.attendanceapp.activities;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 
 import com.d460.attendanceapp.R;
 
 public class PreferenceActivity extends AppCompatActivity {
+    private android.support.v7.widget.Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_preference);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        mToolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
     }
+
 
 }
