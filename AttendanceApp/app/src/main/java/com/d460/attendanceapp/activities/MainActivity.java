c3fdbb1c1c5b1cfc347c30f91f7fb036c04dc1b2
d460 <dagovf@comunidad.unam.mx>
2016-02-06 15:06:26 -0600

    Toolbar propio commit Android Studio

--- AttendanceApp/app/src/main/java/com/d460/attendanceapp/activities/MainActivity.java
+++ AttendanceApp/app/src/main/java/com/d460/attendanceapp/activities/MainActivity.java
@@ -2,14 +2,21 @@ package com.d460.attendanceapp.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import com.d460.attendanceapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Toolbar mToolbar;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mToolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
     }
 }
