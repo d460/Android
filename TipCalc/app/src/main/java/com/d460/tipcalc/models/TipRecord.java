82f674f208277deedbc733ca7739ca41f79e6c18
d460 <dagovf@comunidad.unam.mx>
2016-06-19 15:21:24 -0500

    commit fragment

---
+++ TipCalc/app/src/main/java/com/d460/tipcalc/models/TipRecord.java
@@ -0,0 +1,46 @@
+package com.d460.tipcalc.models;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Created by Dagoberto on 19/06/2016.
+ */
+public class TipRecord {
+    private double bill;
+    private int tipPercentage;
+    private Date timestamp;
+
+    public double getBill() {
+        return bill;
+    }
+
+    public void setBill(double bill) {
+        this.bill = bill;
+    }
+
+    public int getTipPercentage() {
+        return tipPercentage;
+    }
+
+    public void setTipPercentage(int tipPercentage) {
+        this.tipPercentage = tipPercentage;
+    }
+
+    public Date getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Date timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public double getTip(){
+        return bill*(tipPercentage/100d);
+    }
+
+    public String getDateFormat(){
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("MMM dd,yyyy HH:mm");
+        return simpleDateFormat.format(timestamp);
+    }
+}
