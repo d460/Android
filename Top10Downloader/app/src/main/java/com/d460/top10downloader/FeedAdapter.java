7acdda54f24b017e709b38282252bb7eb8ca7f67
d460 <dagovf@comunidad.unam.mx>
2017-04-09 02:00:46 -0500

    FeedAdapter

---
+++ Top10Downloader/app/src/main/java/com/d460/top10downloader/FeedAdapter.java
@@ -0,0 +1,54 @@
+package com.d460.top10downloader;
+
+import android.content.Context;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Created by Dagoberto on 08/04/2017.
+ */
+
+public class FeedAdapter extends ArrayAdapter {
+
+    private static final String TAG = "FeedAdapter";
+    private final int layoutResourse;
+    private final LayoutInflater layoutInflater;
+    private List<FeedEntry> applications;
+
+    public FeedAdapter(@NonNull Context context, @LayoutRes int resource, List<FeedEntry> applications) {
+        super(context, resource);
+        this.layoutResourse = resource;
+        this.layoutInflater = LayoutInflater.from(context);
+        this.applications = applications;
+    }
+
+    @Override
+    public int getCount() {
+        return applications.size();
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+
+        View view = layoutInflater.inflate(layoutResourse, parent, false);
+        TextView tvName = (TextView) view.findViewById(R.id.tvName);
+        TextView tvArtist = (TextView) view.findViewById(R.id.tvArtist);
+        TextView tvSummary = (TextView) view.findViewById(R.id.tvSummary);
+
+        FeedEntry currentApp = applications.get(position);
+        tvName.setText(currentApp.getName());
+        tvArtist.setText(currentApp.getArtist());
+        tvSummary.setText(currentApp.getSummary());
+
+        return view;
+    }
+}
